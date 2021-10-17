b8b324c4042be748af4012726c66b8a83bb74ed4
khanssa1303 <khansavina3@gmail.com>
2021-10-17 21:45:20 +0700

    mengerjakan modul pertemuan 2 membuat contoh1

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>perkenalkan</h1>";
+        echo"Nama saya Vina Fauziah Khansa
+            Saya tinggal di daerah Tasikmalaya
+            olah raga yang saya sukai adalah
+            Bulutangkis";
+    }
+}
